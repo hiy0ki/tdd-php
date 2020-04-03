b7392245bdecd15bd3fe02efe62a692de88f0715
hiyoki <maha.tree.21@gmail.com>
2020-04-03 16:43:22 +0900

    ch.11

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -19,17 +19,8 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
         $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
-        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
-        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
 
         $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
-    }
-
-    public function test_franc_multiplication()
-    {
-        $five = Money::franc(5);
-        $this->assertTrue((Money::franc(10))->equals($five->times(2)));
-        $this->assertTrue((Money::franc(15))->equals($five->times(3)));
     }
 
     public function test_currency()
